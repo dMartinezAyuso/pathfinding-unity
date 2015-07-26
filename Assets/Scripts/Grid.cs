ebebe1e63d6187758af6807a40042a466d7252b5
dMartinezAyuso <diego.martinez.ayuso@gmail.com>
2015-07-26 22:47:21 +0200

    comments deleted

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 
 public class Grid : MonoBehaviour {
 
-	//public bool onlyDisplayPathGizmos; // //**
-	public bool displayGridGizmos;//**
+	public bool displayGridGizmos;
 	public LayerMask unwalkableMask;
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
@@ -14,8 +13,7 @@ public class Grid : MonoBehaviour {
 	float nodeDiameter;
 	int gridSizeX, gridSizeY;
 
-	//void Start(){//**
-	void Awake() {//**
+	void Awake() {
 		nodeDiameter = nodeRadius*2;
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
@@ -35,7 +33,7 @@ public class Grid : MonoBehaviour {
 		}
 	}
 
-	public int MaxSize { //
+	public int MaxSize { 
 		get {
 			return gridSizeX * gridSizeY;
 		}
@@ -72,29 +70,15 @@ public class Grid : MonoBehaviour {
 		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
 		return grid[x,y];
 	}
-	
-	//public List<Node> path;//**
 
 	void OnDrawGizmos() {
 		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
 
-		/*if (onlyDisplayPathGizmos) { // //**
-			if (path != null) { //
-				foreach (Node n in path) {// 
-					Gizmos.color = Color.black;//
-					Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter - .1f));//
-				}
+		if (grid != null && displayGridGizmos) {
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+				Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter - .1f));
 			}
-		} else { //*/
-			if (grid != null && displayGridGizmos) {//**
-				foreach (Node n in grid) {
-					Gizmos.color = (n.walkable) ? Color.white : Color.red;
-					/*if (path != null) //**
-						if (path.Contains (n))
-							Gizmos.color = Color.black;*/
-					Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter - .1f));
-				}
-			}
-		//}//
+		}
 	}
 }
--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -2,65 +2,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System; //**
+using System; 
 
 public class Pathfinding : MonoBehaviour {
 	
-	//public Transform seeker, target;//**
-	PathRequestManager requestManager; //**
+	PathRequestManager requestManager;
 	Grid grid;
 	
 	void Awake() {
-		requestManager = GetComponent<PathRequestManager> ();//**
+		requestManager = GetComponent<PathRequestManager> ();
 		grid = GetComponent<Grid>();
 	}
-	
-	/*void Update() { //**
-		if (Input.GetButtonDown ("Jump")) {//
-			FindPath (seeker.position, target.position);
-		}
-	}*/
 
-	public void StartFindPath(Vector3 startPos, Vector3 targetPos) { //**
+	public void StartFindPath(Vector3 startPos, Vector3 targetPos) {
 		StartCoroutine(FindPath(startPos, targetPos));
 	}
-	
-	//void FindPath(Vector3 startPos, Vector3 targetPos) {
-	IEnumerator FindPath(Vector3 startPos, Vector3 targetPos) { //**
-		Stopwatch sw = new Stopwatch ();//
-		sw.Start ();//
 
-		Vector3[] waypoints = new Vector3[0];//**
-		bool pathSuccess = false; //**
+	IEnumerator FindPath(Vector3 startPos, Vector3 targetPos) {
+		Stopwatch sw = new Stopwatch ();
+		sw.Start ();
+
+		Vector3[] waypoints = new Vector3[0];
+		bool pathSuccess = false; 
 
 		Node startNode = grid.NodeFromWorldPoint(startPos);
 		Node targetNode = grid.NodeFromWorldPoint(targetPos);
 
-		if (startNode.walkable && targetNode.walkable) { //**		
-			//List<Node> openSet = new List<Node>();
-			Heap<Node> openSet = new Heap<Node> (grid.MaxSize); //
+		if (startNode.walkable && targetNode.walkable) { 		
+			Heap<Node> openSet = new Heap<Node> (grid.MaxSize);
 			HashSet<Node> closedSet = new HashSet<Node> ();
 			openSet.Add (startNode);
 			
 			while (openSet.Count > 0) {
-				//Node currentNode = openSet[0];
-				Node currentNode = openSet.RemoveFirst (); //
-				/*for (int i = 1; i < openSet.Count; i ++) {
-					if (openSet[i].fCost <= currentNode.fCost && openSet[i].hCost < currentNode.hCost) {
-						currentNode = openSet[i];
-					}
-				}
-				
-				openSet.Remove(currentNode);*/
+				Node currentNode = openSet.RemoveFirst (); 
 				closedSet.Add (currentNode);
 				
 				if (currentNode == targetNode) {
-					sw.Stop ();//
-					print ("Path found:" + sw.ElapsedMilliseconds + " ms");//
-					pathSuccess = true;//**
-					//RetracePath (startNode, targetNode);//**
-					//return; //**
-					break;//**
+					sw.Stop ();
+					print ("Path found:" + sw.ElapsedMilliseconds + " ms");
+					pathSuccess = true;
+
+					break;
 				}
 				
 				foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
@@ -76,19 +58,18 @@ public class Pathfinding : MonoBehaviour {
 						
 						if (!openSet.Contains (neighbour))
 							openSet.Add (neighbour);
-						openSet.UpdateItem (neighbour); //
+						openSet.UpdateItem (neighbour); 
 					}
 				}
 			}
 		}
-		yield return null; //**
-		if (pathSuccess) { //**
-			waypoints = RetracePath(startNode, targetNode);//**
+		yield return null; 
+		if (pathSuccess) { 
+			waypoints = RetracePath(startNode, targetNode);
 		}
-		requestManager.FinishedProcessingPath (waypoints, pathSuccess);//**
+		requestManager.FinishedProcessingPath (waypoints, pathSuccess);
 	}
 	
-	//void RetracePath(Node startNode, Node endNode) {//**
 	Vector3[] RetracePath(Node startNode, Node endNode) {
 		List<Node> path = new List<Node>();
 		Node currentNode = endNode;
@@ -97,17 +78,12 @@ public class Pathfinding : MonoBehaviour {
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
-		Vector3[] waypoints =  SimplifyPath (path); //**
-		//path.Reverse(); //**
-		Array.Reverse (waypoints);//**
-		
-		//grid.path = path;//**
-
+		Vector3[] waypoints =  SimplifyPath (path); 
+		Array.Reverse (waypoints);
 		return waypoints;
-
 	}
 
-	Vector3[] SimplifyPath(List<Node> path) { //**
+	Vector3[] SimplifyPath(List<Node> path) {
 		List<Vector3> waypoints = new List<Vector3>();
 		Vector2 directionOld = Vector2.zero;
 		
